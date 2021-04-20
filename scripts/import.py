9d9e74c300f63b8f66aa75fcf3dd6fbf45007f2e
Brock Fanning <brockfanning@gmail.com>
2021-04-19 22:06:09 -0400

    Initial commit

---
+++ scripts/import.py
@@ -0,0 +1,13 @@
+import sdmx
+import yaml
+import os
+from sdg.translations import TranslationInputSdmx
+
+translation_input = TranslationInputSdmx(source='SDG_DSD.xml')
+translation_input.execute()
+translations = translation_input.get_translations()
+for language in translations:
+    for concept in translations[language]:
+        filename = os.path.join('translations', language, concept) + '.yml'
+        with open(filename, 'w', encoding='utf-8') as stream:
+            yaml.dump(translations[language][concept], stream)
