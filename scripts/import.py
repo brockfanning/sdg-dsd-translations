0615d0003690ed46126165a35018da5ab124642d
Brock Fanning <brockfanning@gmail.com>
2021-06-23 23:47:35 -0400

    Structure fixes and zh_Hans fix

--- scripts/import.py
+++ scripts/import.py
@@ -3,11 +3,16 @@ import yaml
 import os
 from sdg.translations import TranslationInputSdmx
 
+language_fixes = {
+    'zh': 'zh_Hans'
+}
+
 translation_input = TranslationInputSdmx(source='dsd.xml')
 translation_input.execute()
 translations = translation_input.get_translations()
 for language in translations:
     for concept in translations[language]:
-        filename = os.path.join('translations', language, concept) + '.yml'
+        fixed_language = language_fixes[language] if language in language_fixes else language
+        filename = os.path.join('translations', fixed_language, concept) + '.yml'
         with open(filename, 'w', encoding='utf-8') as stream:
             yaml.dump(translations[language][concept], stream, allow_unicode=True, width=1000)
